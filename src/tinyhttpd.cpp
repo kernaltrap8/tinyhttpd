67e49eecc483c1899febb1854d7d3605a682ee73
kernaltrap <kernaltrap@gmail.com>
2024-07-24 22:21:07 -0500

    Fix typo
    Signed-off-by: kernaltrap <kernaltrap@gmail.com>

--- src/tinyhttpd.cpp
+++ src/tinyhttpd.cpp
@@ -660,7 +660,7 @@ int main(int argc, char *argv[]) {
     tinyhttpd::AddBlacklistedPaths(arguments["-blacklist"]);
   }
 
-  if (arguments.count("-r") > 0 || arguents.count("--rate-limit")) {
+  if (arguments.count("-r") > 0 || arguments.count("--rate-limit")) {
     tinyhttpd::enableRateLimit = true;
   }
 
